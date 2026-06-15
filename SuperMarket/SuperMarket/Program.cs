11ad9c689f18b4972a599817237d5d9cf84c1344
irsathameen <irsath@psibertech.net>
2026-06-15 20:39:38 +0530

    EF Code first has been implemented

--- SuperMarket/SuperMarket/Program.cs
+++ SuperMarket/SuperMarket/Program.cs
@@ -1,17 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SuperMarket.Context;
+using SuperMarket.Data.Interfaces;
+using SuperMarket.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Configure Swagger/OpenAPI (Swashbuckle)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+// Configure Entity Framework Core with SQL Server
+builder.Services.AddDbContext<SuperMarketDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
